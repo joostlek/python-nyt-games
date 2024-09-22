b7301e8df32e5c08104db81c5f69caec392a0c5b
Joost Lekkerkerker <joostlek@outlook.com>
2024-09-22 20:34:31 +0200

    Add unauthorized check (#8)

--- src/nyt_games/exceptions.py
+++ src/nyt_games/exceptions.py
@@ -5,6 +5,10 @@ class NYTGamesError(Exception):
     """Generic exception."""
 
 
+class NYTGamesAuthenticationError(NYTGamesError):
+    """NYT Games authentication exception."""
+
+
 class NYTGamesConnectionError(NYTGamesError):
     """NYT Games connection exception."""
 
